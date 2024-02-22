2cac5cb5da99ec6b150bd3c49a4ea34049000765
cynthiazhi <cynthiazhi@cynthias-mbp.dhcp.wustl.edu>
2024-02-22 11:04:23 -0600

    studio 4

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -18,21 +18,46 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
+		Scanner in = new Scanner(f); // making Scanner with a File
 
 		String shapeType = in.next();
 		int redComponent = in.nextInt();
 		int greenComponent = in.nextInt();
 		int blueComponent = in.nextInt();
 		boolean isFilled = in.nextBoolean();
-		double x = in.nextDouble(); 
-		double y = in.nextDouble();
-		double halfWidth = in.nextDouble();
-		double halfHeigh = in.nextDouble(); 
+		double d1 = in.nextDouble();
+		double d2 = in.nextDouble();
+		double d3 = in.nextDouble();
+		double d4 = in.nextDouble();
+		
 
-		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
-		StdDraw.filledRectangle(x, y, halfWidth, halfHeigh);
+		if (shapeType.equals("rectangle")) {
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			StdDraw.filledRectangle(d1, d2, d3, d4);
+		} else if (shapeType.equals("ellipse")) {
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if (isFilled == true) {
+				StdDraw.filledEllipse(d1, d2, d3, d4);
+			} else {
+				StdDraw.ellipse(d1, d2, d3, d4);
+			}
+		} else if (shapeType.equals("triangle")) {
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			double d5 = in.nextDouble();
+			double d6 = in.nextDouble();
+			if (isFilled == true) {
+				double[] xs = { d1, d3, d5 };
+				double[] ys = { d2, d4, d6 };
+				StdDraw.filledPolygon(xs, ys);
+			} else {
+				double[] xs = { d1, d3, d5 };
+				double[] ys = { d2, d4, d6 };
+				StdDraw.polygon(xs, ys);
+
+			}
+		}
 	}
 }
+
 //purple outline ellipse 73 0 146 false 0.5 0.5 0.2 0.2
 //rectangle 0 0 0 false 0.5 0.5 0.2 0.2
